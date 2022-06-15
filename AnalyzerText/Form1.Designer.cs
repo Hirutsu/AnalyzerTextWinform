0baded45b97ee145abd7107cf1ac816a65d6f5b0
Levan Burchuladze <levan.burchuladze@rokolabs.com>
2022-06-15 23:23:49 +0400

    release text analyzer

---
+++ AnalyzerText/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace AnalyzerText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.alphabetTB = new System.Windows.Forms.TextBox();
+            this.palindromeTB = new System.Windows.Forms.TextBox();
+            this.AnalyzerBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTB
+            // 
+            this.inputTB.Location = new System.Drawing.Point(12, 25);
+            this.inputTB.Multiline = true;
+            this.inputTB.Name = "inputTB";
+            this.inputTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTB.Size = new System.Drawing.Size(303, 346);
+            this.inputTB.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(454, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Слова по Алфавиту";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Слова палиндром";
+            // 
+            // alphabetTB
+            // 
+            this.alphabetTB.Location = new System.Drawing.Point(321, 25);
+            this.alphabetTB.Multiline = true;
+            this.alphabetTB.Name = "alphabetTB";
+            this.alphabetTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.alphabetTB.Size = new System.Drawing.Size(364, 181);
+            this.alphabetTB.TabIndex = 4;
+            // 
+            // palindromeTB
+            // 
+            this.palindromeTB.Location = new System.Drawing.Point(321, 225);
+            this.palindromeTB.Multiline = true;
+            this.palindromeTB.Name = "palindromeTB";
+            this.palindromeTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.palindromeTB.Size = new System.Drawing.Size(364, 181);
+            this.palindromeTB.TabIndex = 5;
+            // 
+            // AnalyzerBtn
+            // 
+            this.AnalyzerBtn.Location = new System.Drawing.Point(12, 377);
+            this.AnalyzerBtn.Name = "AnalyzerBtn";
+            this.AnalyzerBtn.Size = new System.Drawing.Size(303, 35);
+            this.AnalyzerBtn.TabIndex = 6;
+            this.AnalyzerBtn.Text = "Проанализировать";
+            this.AnalyzerBtn.UseVisualStyleBackColor = true;
+            this.AnalyzerBtn.Click += new System.EventHandler(this.AnalyzerBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 417);
+            this.Controls.Add(this.AnalyzerBtn);
+            this.Controls.Add(this.palindromeTB);
+            this.Controls.Add(this.alphabetTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputTB);
+            this.Name = "Form1";
+            this.Text = "Analyzer Text";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox alphabetTB;
+        private System.Windows.Forms.TextBox palindromeTB;
+        private System.Windows.Forms.Button AnalyzerBtn;
+    }
+}
+
---
+++ AnalyzerText/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalyzerText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AnalyzerBtn_Click(object sender, EventArgs e)
+        {
+            alphabetTB.Text = String.Join(Environment.NewLine,GetAlphaWords());
+            palindromeTB.Text = String.Join(Environment.NewLine,GetPalindrome());
+        }
+
+        private List<String> GetPalindrome()
+        {
+            List<String> words = new List<String>(GetOnlyTextWithoutSymbols().Split());
+            List<String> palindrome = new List<String>();
+
+            bool flag;
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                flag = true;
+                for (int j = 0; j < words[i].Length / 2; j++)
+                {
+                    string word = words[i];
+                    if (word[j] != word[word.Length - j - 1])
+                    {
+                        flag = false;
+                    }
+                }
+                if (flag)
+                {
+                    palindrome.Add(words[i]);
+                }
+            }
+
+            return palindrome;
+        }
+
+        private List<String> GetAlphaWords()
+        {
+            List<String> words = new List<String>(GetOnlyTextWithoutSymbols().Split());
+            words =  words.Distinct().ToList();
+            words.Sort();
+            return words;
+        }
+        
+        private String GetOnlyTextWithoutSymbols()
+        {
+            string text = inputTB.Text;
+            var newText = Regex.Replace(text, "[-.?!)(,:\n\r]", "");
+
+            return newText;
+        }
+    }
+}
